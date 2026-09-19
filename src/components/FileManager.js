64f775b6eb8204aaa9355ebf93611af7052de45f
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-09-19 17:30:46 -0300

    Enviado pelo SK Code Editor - 19/09/2026

---
+++ src/components/FileManager.js
@@ -0,0 +1,9 @@
+const FileManager = () => {
+    const fileManagerContainer = document.createElement('div');
+    fileManagerContainer.className = 'file-manager';
+    fileManagerContainer.innerHTML = 'Gerenciador de arquivos aqui...';
+
+    return fileManagerContainer;
+};
+
+export default FileManager;
