ab5814264a6eaa6fb28dd63141e6a66fd97884f2
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-09-19 17:30:45 -0300

    Enviado pelo SK Code Editor - 19/09/2026

---
+++ src/components/Preview.js
@@ -0,0 +1,9 @@
+const Preview = () => {
+    const previewContainer = document.createElement('div');
+    previewContainer.className = 'preview';
+    previewContainer.innerHTML = 'Visualização aqui...';
+
+    return previewContainer;
+};
+
+export default Preview;
