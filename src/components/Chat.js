fd689264e60b5bb19573674af625057c2239c373
jorgecaldeira62-gif <jorgecaldeira62@gmail.com>
2026-09-19 17:30:47 -0300

    Enviado pelo SK Code Editor - 19/09/2026

---
+++ src/components/Chat.js
@@ -0,0 +1,9 @@
+const Chat = () => {
+    const chatContainer = document.createElement('div');
+    chatContainer.className = 'chat';
+    chatContainer.innerHTML = 'Chat aqui...';
+
+    return chatContainer;
+};
+
+export default Chat;
